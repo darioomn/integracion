c5117e80af87453ec00c0271fe0bdffb36e4ee45
Thomas Lee <tjl352@Thomass-MacBook-Pro.local>
2016-02-11 21:00:38 -0700

    initial commit

---
+++ src/test/MainTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void mainTest(){
+        Assert.assertTrue(true);
+    }
+}
